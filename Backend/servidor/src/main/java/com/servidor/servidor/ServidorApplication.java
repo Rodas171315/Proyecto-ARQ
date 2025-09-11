d0fd140b9c62c32735ed0ea2c8ed81801dcf7065
Dylan Rodas <rodas171315@unis.edu.gt>
2025-09-10 20:40:18 -0600

    Dockerizando la app servidor

--- Backend/servidor/src/main/java/com/servidor/servidor/ServidorApplication.java
+++ Backend/servidor/src/main/java/com/servidor/servidor/ServidorApplication.java
@@ -2,9 +2,17 @@ package com.servidor.servidor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServidorApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServidorApplication.class, args);
