1b14415a1bbeaec6db2cb2b0dfca319e89451d30
Dylan Rodas <rodas171315@unis.edu.gt>
2025-09-11 16:30:00 -0600

    Mejorando dockerfile del servidor

--- Backend/servidor/src/main/java/com/servidor/servidor/ServidorApplication.java
+++ Backend/servidor/src/main/java/com/servidor/servidor/ServidorApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServidorApplication {
 
-	@RequestMapping("/")
+	@RequestMapping("/hola")
 	public String home() {
 		return "Hello World";
 	}
